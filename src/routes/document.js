8a077dc6cc09c2573de44b9186398a0cda0b5fd1
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-10-25 14:52:39 +0100

    add document routes

---
+++ src/routes/document.js
@@ -0,0 +1,12 @@
+const { fileParser } = require('../middlewares')
+const { auth, isAdmin } = require('../middlewares/auth')
+const router = require('express').Router()
+const controllers = require('./../controllers/document')
+
+router.use(auth)
+router.post('/upload', fileParser.array('documents', 10), controllers.uploadDocument)
+
+router.use(isAdmin)
+router.get('/', controllers.getUploadedDocument)
+
+module.exports = router
