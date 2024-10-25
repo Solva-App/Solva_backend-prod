15458ab2af8e5dba7c616480d589338358d565d9
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-10-25 14:34:57 +0100

    setup the Freelancer  model

---
+++ src/models/Freelancer.js
@@ -0,0 +1,62 @@
+const { DataTypes } = require('sequelize')
+const { sequelize } = require('../database/db')
+const { STRING, INTEGER, BOOLEAN, TEXT } = DataTypes
+
+const freelancerSchema = {
+    owner: {
+        type: INTEGER,
+        allowNull: false,
+    },
+    fullName: {
+        type: STRING,
+        allowNull: false,
+    },
+    category: {
+        type: STRING,
+        allowNull: false,
+    },
+    bio: {
+        type: TEXT,
+        allowNull: false,
+    },
+    startingAmount: {
+        type: STRING,
+        allowNull: false,
+    },
+    portfolioLink: {
+        type: STRING,
+        defaultValue: 'awaiting-response',
+    },
+    phoneNumber: {
+        type: STRING,
+        allowNull: false,
+    },
+    whatsappLink: {
+        type: STRING,
+        allowNull: false,
+    },
+    profilePic: {
+        type: STRING,
+        allowNull: false,
+    },
+}
+
+const Freelancer = sequelize.define('Freelancer', freelancerSchema, {
+    timestamps: true,
+    hooks: {
+        beforeValidate(_pl) {},
+        beforeUpdate(_pl) {},
+        afterFind(_pl) {},
+    },
+})
+
+// sync database
+Freelancer.sync({ alter: true })
+    .then((_) => {
+        if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'production') console.log('=> Freelancer model synced ✔️')
+    })
+    .catch((_) => {
+        if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'production') console.log('Error while syncing Freelancer❌')
+    })
+
+module.exports = Freelancer
