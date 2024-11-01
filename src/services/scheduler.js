8fcd89adf1af396c92445fa0d19fa5f3279553d7
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2024-11-01 12:30:57 +0100

    Add scheduler to manage automatic charge

---
+++ src/services/scheduler.js
@@ -0,0 +1,53 @@
+const schedule = require('node-schedule')
+const paystack = require('./../http/paystack')
+const CustomError = require('../helpers/error')
+const User = require('../models/User')
+const { formatDate } = require('./../helpers/time')
+const Token = require('../models/Token')
+
+const initiateCharge = function (user) {
+    console.log(`Charge will be initiated ${formatDate(user.lastSubscriptionExpiresAt)}`, user.email)
+    return async () => {
+        // console.log('charge in progress')
+        const amount = user.lastSubscriptionPlan === 'premium' ? 1999 : 999
+        // initiate the charge
+        const charge = await paystack.chargeCard({
+            email: user.email,
+            amount: amount,
+            authorization_code: user.chargeAuthCode,
+            metadata: {
+                id: user.id,
+                type: user.lastSubscriptionPlan,
+            },
+        })
+
+        if (charge instanceof CustomError) {
+            user.willChargeAgain = false
+        }
+
+        return await user.save()
+    }
+}
+
+module.exports.initiateSubscriptionScheduler = async function (user) {
+    schedule.scheduleJob(user.chargeAuthCode, user.lastSubscriptionExpiresAt, initiateCharge(user))
+}
+
+module.exports.initiateAllSubscriptionScheduler = async function () {
+    const users = await User.findAll({
+        where: {
+            autoCharge: true,
+            chargeChannel: 'card',
+        },
+    })
+
+    // initiate all user charge
+    users.forEach((user) => schedule.scheduleJob(user.chargeAuthCode, user.lastSubscriptionExpiresAt, initiateCharge(user)))
+}
+
+module.exports.stopAutoCharge = async function (user) {
+    console.log('Auto Charge Disactivated Successfully')
+    user.autoCharge = false
+    await user.save()
+    return schedule.cancelJob(user.chargeAuthCode)
+}
