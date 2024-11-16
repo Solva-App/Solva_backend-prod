fb961aff7e026483346293dc9204882bd83a5436
bytegrn <greenbrightsmart@gmail.com>
2024-11-16 18:24:36 +0100

    dockerize the app

---
+++ README.md
@@ -0,0 +1,27 @@
+# Server setup instructions
+
+This guide contains instructions on running the server locally, thereby replicating the production enviroment.
+
+## Prerequisites
+
+Ensure you have docker installed and running on your development machine
+
+## Seeting up the server
+
+Clone the server repo to your local machine usinig Git:
+
+`https://github.com/bytegrn/solva-backend.git`
+
+`cd` solva-backend
+
+## Build and start the server
+
+`docker-compose up --build`
+
+## Run docker in detached mode
+
+`docker-compose up -d`
+
+## Access the application
+
+[server running at](http://localhost:3002)
--- src/database/db.js
+++ src/database/db.js
@@ -1,40 +1,42 @@
-const { Sequelize, DataTypes } = require('sequelize')
+const { Sequelize, DataTypes } = require("sequelize");
 
-const databasename = process.env.DATABASE_NAME
-const username = process.env.DATABASE_USERNAME
-const password = process.env.DATABASE_PASSWORD
-const hostAddress = process.env.DATABASE_HOSTNAME
+const databasename = process.env.DATABASE_NAME;
+const username = process.env.DATABASE_USERNAME;
+const password = process.env.DATABASE_PASSWORD;
+const hostAddress = process.env.DATABASE_HOSTNAME;
+
+console.log(username);
 
 // Create the sequelize instance
 const sequelize = new Sequelize(databasename, username, password, {
-    host: hostAddress,
-    dialect: 'mysql',
-    logging: false,
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000,
-    },
-})
+  host: hostAddress,
+  dialect: "mysql",
+  logging: false,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+});
 
 const startDB = async function (services) {
-    sequelize
-        .sync(/* { alter: true } */)
-        .then(async () => {
-            // start services
-            services.forEach((service) => {
-                const result = service()
+  sequelize
+    .sync(/* { alter: true } */)
+    .then(async () => {
+      // start services
+      services.forEach((service) => {
+        const result = service();
 
-                if (result instanceof Promise)
-                    // check if value is a promise
-                    result.then((_) => {}).catch((_) => console.log(_))
-            })
-            console.error('app connected to mySQL server 🚀')
-        })
-        .catch(async (error) => {
-            console.log(error)
-            console.error('Unable to connect to database', '\n -----------------------------')
-        })
-}
+        if (result instanceof Promise)
+          // check if value is a promise
+          result.then((_) => {}).catch((_) => console.log(_));
+      });
+      console.error("app connected to mySQL server 🚀");
+    })
+    .catch(async (error) => {
+      console.log(error);
+      console.error("Unable to connect to database", "\n -----------------------------");
+    });
+};
 
-module.exports = { startDB, sequelize }
+module.exports = { startDB, sequelize };
