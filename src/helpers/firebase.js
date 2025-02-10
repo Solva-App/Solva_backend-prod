b87dcebc76b526e6b942fec307fc3ecbb59150b3
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-10 17:34:16 +0100

    format code

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -7,14 +7,14 @@ app.initializeApp(firebaseKeys.storage)
 
 const bucket = storage.getStorage()
 
-module.exports.fileUpload = async function (file, location) {
-    try {
-        const metadata = { contentType: file.mimetype }
-        const ref = storage.ref(bucket, `${location}/${Math.round(Math.random() * 1e9)}-${file.originalname} `)
-        const snapshort = await storage.uploadBytes(ref, file.buffer, metadata)
-        return await storage.getDownloadURL(snapshort.ref)
-    } catch (error) {
-        console.log(error)
-        return CustomError.internalServerError('Something went wrong', error)
-    }
+module.exports.fileUpload = async function(file, location) {
+      try {
+            const metadata = { contentType: file.mimetype }
+            const ref = storage.ref(bucket, `${location}/${Math.round(Math.random() * 1e9)}-${file.originalname} `)
+            const snapshort = await storage.uploadBytes(ref, file.buffer, metadata)
+            return await storage.getDownloadURL(snapshort.ref)
+      } catch (error) {
+            console.log(error)
+            return CustomError.internalServerError('Something went wrong', error)
+      }
 }
