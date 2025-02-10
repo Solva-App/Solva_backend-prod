70fe51b4ffa8339efa638bf726d8ba508504581b
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-02-10 17:34:55 +0100

    format code and fix getQeustion.. controllers

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -8,145 +8,130 @@ const { OK } = require('http-status-codes')
 const Question = require('../models/Question')
 const { Op } = require('sequelize')
 
-module.exports.createPastQuestion = async function (req, res, next) {
-    try {
-        const schema = new Schema({
-            title: { type: 'string', required: true },
-            department: { type: 'string', required: true },
-            university: { type: 'string', required: true },
-            department: { type: 'string', required: true },
-            courseCode: { type: 'string', required: true },
-            faculty: { type: 'string', required: true },
-            documents: { type: 'array', required: false },
-        })
-        req.body.documents = []
+module.exports.createPastQuestion = async function(req, res, next) {
+      try {
+            const schema = new Schema({
+                  title: { type: 'string', required: true },
+                  department: { type: 'string', required: true },
+                  university: { type: 'string', required: true },
+                  department: { type: 'string', required: true },
+                  courseCode: { type: 'string', required: true },
+                  faculty: { type: 'string', required: true },
+                  documents: { type: 'array', required: false },
+            })
+            req.body.documents = []
 
-        let { body } = req
-        let files = {
-            documents: [],
-        }
-
-        for (let file of req.files) {
-            file = file
-            files[file.fieldname].push(await image.modifyStringImageFile(body[file.fieldname].length ? body[file.fieldname] : file))
-        }
-
-        const result = schema.validate({ ...body, ...files })
-        if (result.error) {
-            return next(CustomError.badRequest('Invalid request body', result.error))
-        }
-
-        // upload cert to firebase or aws bucket
-        for (const file of files.documents) {
-            const upload = await firebase.fileUpload(file, file.fieldname)
-            if (upload instanceof CustomError) {
-                return next(upload)
+            let { body } = req
+            let files = {
+                  documents: [],
             }
-            body[file.fieldname].push({
-                size: file.size,
-                url: upload,
+
+            for (let file of req.files) {
+                  file = file
+                  files[file.fieldname].push(await image.modifyStringImageFile(body[file.fieldname].length ? body[file.fieldname] : file))
+            }
+
+            const result = schema.validate({ ...body, ...files })
+            if (result.error) {
+                  return next(CustomError.badRequest('Invalid request body', result.error))
+            }
+
+            // upload cert to firebase or aws bucket
+            for (const file of files.documents) {
+                  const upload = await firebase.fileUpload(file, file.fieldname)
+                  if (upload instanceof CustomError) {
+                        return next(upload)
+                  }
+                  body[file.fieldname].push({
+                        size: file.size,
+                        url: upload,
+                  })
+            }
+
+            const question = await Question.create({
+                  ...body,
+                  documents: undefined,
+                  owner: req.user.id,
             })
-        }
 
-        const question = await Question.create({
-            ...body,
-            documents: undefined,
-            owner: req.user.id,
-        })
+            const documents = await Document.bulkCreate(
+                  body.documents.map((d) => {
+                        return {
+                              model: 'question',
+                              owner: req.user.id,
+                              modelId: question.id,
+                              url: d.url,
+                              size: d.size,
+                              status: 'approved',
+                              requiresApproval: false,
+                        }
+                  })
+            )
 
-        const documents = await Document.bulkCreate(
-            body.documents.map((d) => {
-                return {
-                    model: 'question',
-                    owner: req.user.id,
-                    modelId: question.id,
-                    url: d.url,
-                    size: d.size,
-                    status: 'approved',
-                    requiresApproval: false,
-                }
+            res.status(OK).json({
+                  success: true,
+                  status: res.statusCode,
+                  message: 'Question created successfully',
+                  data: {
+                        question,
+                        documents,
+                  },
             })
-        )
-
-        res.status(OK).json({
-            success: true,
-            status: res.statusCode,
-            message: 'Question created successfully',
-            data: {
-                question,
-                documents,
-            },
-        })
-    } catch (error) {
-        return next({ error })
-    }
+      } catch (error) {
+            return next({ error })
+      }
 }
 
-module.exports.getpastQuestions = async function (req, res, next) {
-    try {
-        let body
+module.exports.getPastQuestions = async function(req, res, next) {
+      try {
 
-        if (req.query.search) {
-            body = {
-                where: {
-                    // ...req.body.filter,
-                    [Op.or]: [
-                      { title: { [Op.like]: `%${req.query.search}%` } }, { university: { [Op.like]: `%${req.query.search}%` } }, { faculty: { [Op.like]: `%${req.query.search}%` } }, { department: { [Op.like]: `%${req.query.search}%` } }, { courseCode: { [Op.like]: `%${req.query.search}%` } }],
-                },
-            }
-        } else {
-            body = {}
-        }
+            const schema = new Schema({
+                  university: { type: 'string', required: true },
+                  department: { type: 'string', required: true },
+                  faculty: { type: 'string', required: true },
+            })
 
-        let questions = await Question.findAll(body)
-        questions = questions.map(async (question) => {
-            const docs = await Document.findAll({ where: { model: 'question', modelId: question.id } })
-            return {
-                question: question,
-                document: docs,
-            }
-        })
+            let questions = await Question.findAll({
+                  where: { ...req.query }
+            })
 
-        res.status(OK).json({
-            success: true,
-            status: res.statusCode,
-            data: await Promise.all(questions),
-        })
-    } catch (error) {
-        return next({ error })
-    }
+            questions = questions.map(async (question) => {
+                  const docs = await Document.findAll({ where: { model: 'question', modelId: question.id } })
+                  return {
+                        question: question,
+                        document: docs,
+                  }
+            })
+
+            res.status(OK).json({
+                  success: true,
+                  status: res.statusCode,
+                  data: await Promise.all(questions),
+            })
+
+      } catch (error) {
+            return next({ error })
+      }
 }
 
-module.exports.getPastQuestion = async function (req, res, next) {
-    try {
-
-        const schema = new Schema({
-            department: { type: 'string', required: true },
-            university: { type: 'string', required: true },
-            department: { type: 'string', required: true },
-            courseCode: { type: 'string', required: true },
-            faculty: { type: 'string', required: true },
-        })
-
-        let questions = await Question.findAll({
-          where: { ...req.query }
-        })
-
-        questions = questions.map(async (question) => {
-            const docs = await Document.findAll({ where: { model: 'question', modelId: question.id } })
-            return {
-                question: question,
-                document: docs,
+module.exports.getPastQuestion = async function(req, res, next) {
+      try {
+            const question = await Question.findByPk(req.params.id)
+            if (!question) {
+                  return next(CustomError.badRequest("Question does not exist"))
             }
-        })
 
-        res.status(OK).json({
-            success: true,
-            status: res.statusCode,
-            data: await Promise.all(questions),
-        })
+            res.status(OK).json({
+                  success: true,
+                  status: res.statusCode,
+                  message: "Question fetched successfully",
+                  data: {
+                        question,
+                        documents: await Document.findAll({ where: { owner: question.id } })
+                  }
+            })
 
-    } catch (error) {
-        return next({ error })
-    }
+      } catch (error) {
+            return next({ error })
+      }
 }
