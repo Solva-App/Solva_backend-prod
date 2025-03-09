475aae711b861df7066104a68c90899c7b36b0ef
Paul Ezekiel-Hart <hartpaulisimo@gmail.com>
2025-03-09 09:24:17 +0100

    work on the file upload on the document controller

--- src/controllers/document.js
+++ src/controllers/document.js
@@ -6,116 +6,115 @@ const Document = require("../models/Document");
 const { OK } = require("http-status-codes");
 const User = require("../models/User");
 
-module.exports.uploadDocument = async function (req, res, next) {
-  try {
-    const schema = new Schema({
-      documents: { type: "array", required: true },
-    });
+module.exports.uploadDocument = async function(req, res, next) {
+     try {
+          const schema = new Schema({
+               documents: { type: "array", required: true },
+          });
 
-    req.body.documents = [];
-    let { body } = req;
-    let files = {
-      documents: [],
-    };
+          req.body.documents = [];
+          let { body } = req;
+          let files = {
+               documents: [],
+          };
 
-    for (let file of req.files) {
-      file = file;
-      files[file.fieldname].push(
-        await image.modifyStringImageFile(body[file.fieldname].length ? body[file.fieldname] : file)
-      );
-    }
+          for (let file of req.files) {
+               file = file;
+               files[file.fieldname].push(
+                    await image.modifyStringImageFile(body[file.fieldname].length ? body[file.fieldname] : file)
+               );
+          }
 
-    const result = schema.validate({ ...body, ...files });
-    if (result.error) {
-      return next(CustomError.badRequest("Invalid request body", result.error));
-    }
+          const result = schema.validate({ ...body, ...files });
+          if (result.error) {
+               return next(CustomError.badRequest("Invalid request body", result.error));
+          }
 
-    // upload cert to firebase or aws bucket
-    console.log(file);
-    for (const file of files.documents) {
-      const upload = await firebase.fileUpload(file, file.fieldname);
-      if (upload instanceof CustomError) {
-        return next(body.documents);
-      }
-      body[file.fieldname].push({
-        size: file.size,
-        url: upload,
-        name: file.originalname,
-        memetype: file.memetype,
-      });
-    }
+          // upload cert to firebase or aws bucket
+          for (const file of files.documents) {
+               const upload = await firebase.fileUpload(file, file.fieldname);
+               if (upload instanceof CustomError) {
+                    return next(body.documents);
+               }
+               body[file.fieldname].push({
+                    size: file.size,
+                    url: upload,
+                    name: file.originalname,
+                    mimetype: file.mimetype,
+               });
+          }
 
-    const documents = await Document.bulkCreate(
-      body.documents.map((d) => {
-        return {
-          model: req.param.filetype,
-          owner: req.user.id,
-          // modelId: ,
-          url: d.url,
-          size: d.size,
-          status: "awaiting-approval",
-          memetype: d.mimetype,
-          name: d.originalname,
-        };
-      })
-    );
+          const documents = await Document.bulkCreate(
+               body.documents.map((d) => {
+                    return {
+                         model: req.param.filetype,
+                         owner: req.user.id,
+                         // modelId: ,
+                         url: d.url,
+                         size: d.size,
+                         status: "awaiting-approval",
+                         mimetype: d.mimetype,
+                         name: d.name,
+                    };
+               })
+          );
 
-    res.status(OK).json({
-      success: true,
-      status: res.statusCode,
-      message: "document created successfully",
-      data: documents,
-    });
+          res.status(OK).json({
+               success: true,
+               status: res.statusCode,
+               message: "document created successfully",
+               data: documents,
+          });
 
-    body = null;
-  } catch (error) {
-    return next({ error });
-  }
+          body = null;
+     } catch (error) {
+          return next({ error });
+     }
 };
 
-module.exports.approveDocument = async function (req, res, next) {
-  try {
-    const document = await Document.findOne({
-      where: {
-        id: req.params.docId,
-        requiresApproval: true,
-      },
-    });
+module.exports.approveDocument = async function(req, res, next) {
+     try {
+          const document = await Document.findOne({
+               where: {
+                    id: req.params.docId,
+                    requiresApproval: true,
+               },
+          });
 
-    if (!document) {
-      return next(CustomError.badRequest("Invalid document id"));
-    } else if (document.approved) {
-      return next(CustomError.badRequest("Document already approved"));
-    }
+          if (!document) {
+               return next(CustomError.badRequest("Invalid document id"));
+          } else if (document.approved) {
+               return next(CustomError.badRequest("Document already approved"));
+          }
 
-    const uploader = await User.findByPk(document.owner);
-    if (!uploader) {
-      return console.log("how is uploader not exiting");
-    }
+          const uploader = await User.findByPk(document.owner);
+          if (!uploader) {
+               return console.log("how is uploader not exiting");
+          }
 
-    const fee = 100;
-    uploader.balance += uploader.balance;
-    await uploader.save();
+          const fee = 100;
+          uploader.balance += uploader.balance;
+          await uploader.save();
 
-    document.status = "approved";
-  } catch (error) {
-    return next({ error });
-  }
+          document.status = "approved";
+     } catch (error) {
+          return next({ error });
+     }
 };
 
-module.exports.getUploadedDocument = async function (req, res, next) {
-  try {
-    const document = await Document.findAll({
-      where: {
-        requiresApproval: true,
-        status: "awaiting-approval",
-      },
-    });
+module.exports.getUploadedDocument = async function(req, res, next) {
+     try {
+          const document = await Document.findAll({
+               where: {
+                    requiresApproval: true,
+                    status: "awaiting-approval",
+               },
+          });
 
-    res.status(OK).json({
-      data: document,
-    });
-  } catch (error) {
-    return next({ error });
-  }
+          res.status(OK).json({
+               data: document,
+          });
+     } catch (error) {
+          return next({ error });
+     }
 };
