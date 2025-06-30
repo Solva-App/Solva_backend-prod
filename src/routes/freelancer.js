45e09feb0016d9e792a25cbabdfd56de0c0d6a68
Fhave <omosiyobo@gmail.com>
2025-06-30 14:48:37 +0100

    fix: edit freelancer
    changed from put to patch

--- src/routes/freelancer.js
+++ src/routes/freelancer.js
@@ -5,12 +5,12 @@ const controllers = require('./../controllers/freelancer')
 
 router.post('/create/comment/:freelancerId', controllers.addComment)
 
-// router.use(auth)
+router.use(auth)
 
 router.post('/create', fileParser.single('profilePic'), controllers.becomeFreelancer)
 router.get('/', controllers.getFreelancers)
 router.get('/catigories', controllers.getCatigories)
 router.get('/:id', controllers.getFreelancer)
-router.put('/edit', fileParser.single('profilePic'), controllers.editFreelancerProfile)
+router.patch('/edit', fileParser.single('profilePic'), controllers.editFreelancerProfile)
 
 module.exports = router
