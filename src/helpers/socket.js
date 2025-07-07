59f02c76f66850c256e76749a17a3bf8a3fdf3f1
Fhave <omosiyobo@gmail.com>
2025-07-07 10:49:21 +0100

    minor corrections

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -5,11 +5,13 @@ module.exports = function setupSocket(io) {
 
   io.on("connection", (socket) => {
     const socketId = socket.id;
+    const userId = socket.user?.id;
+
     socket.join(socketId.toString());
-    console.log(`User ${socketId} connected`);
+    console.log(`User ${userId} connected on Socket ID: ${socketId}`);
 
     socket.on("disconnect", () => {
-      console.log(`User ${socketId} disconnected`);
+      console.log(`User ${userId} disconnected from Socket ID: ${socketId}`);
     });
   });
 };
