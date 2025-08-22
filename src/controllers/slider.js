86de85c492c697fa24c53436b3d5bfc025957c05
Fhave <omosiyobo@gmail.com>
2025-08-22 13:13:27 +0100

    testing

--- src/controllers/slider.js
+++ src/controllers/slider.js
@@ -6,98 +6,92 @@ const firebase = require('./../helpers/firebase')
 const CustomError = require('../helpers/error')
 
 module.exports.uploadSlideImage = async function (req, res, next) {
-  try {
-    const schema = new Schema({
-      slides: { type: 'array', required: true },
-    })
-
-    req.body.slides = []
-    let { body } = req
-    let files = {
-      slides: [],
-    }
-
-    for (let file of req.files) {
-      file = file
-      console.log(file.fieldname)
-      files[file.fieldname].push(
-        await image.modifyStringImageFile(body[file.fieldname].length ? body[file.fieldname] : file)
-      );
-    }
-
-    const result = schema.validate({ ...body, ...files })
-    if (result.error) {
-      return next(CustomError.badRequest('Invalid request body', result.error))
-    }
-
-    const images = await Slide.findAll({})
-    if (images.length + files.slides.length > 4) {
-      return next(CustomError.badRequest('You can only have 4 images'))
-    }
-
-    // upload cert to firebase or aws bucket
-    for (const file of files.slides) {
-      try {
-        const upload = await firebase.fileUpload(file, file.fieldname)
-        if (upload instanceof CustomError) {
-          return next(body.slides)
-        }
-        body[file.fieldname].push({
-          size: file.size,
-          url: upload,
+    try {
+        const schema = new Schema({
+            slides: { type: 'array', required: true },
         })
-      } catch (error) {
-        return next(error)
-      }
+
+        req.body.slides = []
+        let { body } = req
+        let files = {
+            slides: [],
+        }
+
+        for (let file of req.files) {
+            file = file
+            console.log(file.fieldname)
+            files[file.fieldname].push(await image.modifyStringImageFile(body['slides'].length ? body[file.fieldname] : file))
+        }
+
+        const result = schema.validate({ ...body, ...files })
+        if (result.error) {
+            return next(CustomError.badRequest('Invalid request body', result.error))
+        }
+
+        const images = await Slide.findAll({})
+        if ([...images, ...files.slides].length > 4) {
+            return next(CustomError.badRequest('You can only have 4 images'))
+        }
+
+        // upload cert to firebase or aws bucket
+        for (const file of files.slides) {
+            const upload = await firebase.fileUpload(file, file.fieldname)
+            if (upload instanceof CustomError) {
+                return next(body.slides)
+            }
+            body[file.fieldname].push({
+                size: file.size,
+                url: upload,
+            })
+        }
+
+        const slides = await Slide.bulkCreate(
+            body.slides.map((slide) => ({
+                owner: req.user.id,
+                size: slide.size,
+                url: slide.url,
+            }))
+        )
+
+        res.status(OK).json({
+            success: true,
+            status: res.statusCode,
+            message: 'image uploaded successfully',
+            data: slides,
+        })
+    } catch (error) {
+        return next({ error })
     }
-
-    const slides = await Slide.bulkCreate(
-      body.slides.map((slide) => ({
-        owner: req.user.id,
-        size: slide.size,
-        url: slide.url,
-      }))
-    )
-
-    res.status(OK).json({
-      success: true,
-      status: res.statusCode,
-      message: 'image uploaded successfully',
-      data: slides,
-    })
-  } catch (error) {
-    return next({ error })
-  }
 }
 
 module.exports.getSlide = async function (req, res, next) {
-  try {
-    const slides = await Slide.findAll({})
-    res.status(OK).json({
-      success: true,
-      status: res.statusCode,
-      message: 'Fetch all slide',
-      data: slides,
-    })
-  } catch (error) {
-    return next(error)
-  }
+    try {
+        const slides = await Slide.findAll({})
+        res.status(OK).json({
+            success: true,
+            status: res.statusCode,
+            message: 'Fetch all slide',
+            data: slides,
+        })
+    } catch (error) {
+        return next(error)
+    }
 }
 
 module.exports.deleteSlide = async function (req, res, next) {
-  try {
-    const slide = await Slide.destroy({ where: { id: req.params.id } })
-    if (!slide) {
-      return next(CustomError.badRequest('Invalid slide id'))
-    }
+    try {
+        const slide = await Slide.destroy({ where: { id: req.params.id } })
+        if (!slide) {
+            return next(CustomError.badRequest('Invalid slide id'))
+        }
 
-    res.status(OK).json({
-      success: true,
-      status: res.statusCode,
-      message: 'Slide delete successfully',
-      data: slide,
-    })
-  } catch (error) {
-    return next({ error })
-  }
+        res.status(OK).json({
+            success: true,
+            status: res.statusCode,
+            message: 'Slide delete successfully',
+            data: slide,
+        })
+    } catch (error) {
+        return next({ error })
+    }
 }
