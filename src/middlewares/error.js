ba5a6faba0f26e63a41d74a7169650c01be5dc2d
Fhave <omosiyobo@gmail.com>
2026-03-08 01:17:05 +0100

    fix: add time to error logging

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,22 +1,34 @@
 const CustomError = require('./../helpers/error')
+const now = new Date();
+const watTime = new Intl.DateTimeFormat('en-GB', {
+  dateStyle: 'short',
+  timeStyle: 'medium',
+  timeZone: 'Africa/Lagos'
+}).format(now);
 
 module.exports = function (error, req, res, _next) {
-    error = error.instance ? error.instance : error
+  error = error.instance ? error.instance : error
 
-    console.error('----------------------------------------------------------------')
-    console.error('Error triggered by', req?.user?.fullName ?? req?.user?.username ?? ' none auth user', 'with ID of', req?.user?.id, '\n\n')
-    console.error(req.body)
-    console.error(error)
-    console.error('----------------------------------------------------------------')
+  console.error('----------------------------------------------------------------')
+  console.error(
+    `[${watTime} WAT] Error triggered by`,
+    req?.user?.fullName ?? req?.user?.username ?? 'none auth user',
+    'with ID of',
+    req?.user?.id,
+    '\n\n'
+  )
+  console.error(req.body)
+  console.error(error)
+  console.error('----------------------------------------------------------------')
 
-    if (error instanceof CustomError) {
-        return res.status(error.status).send(error)
-    }
+  if (error instanceof CustomError) {
+    return res.status(error.status).send(error)
+  }
 
-    return res.status(500).send({
-        message: 'something went wrong',
-        status: 500,
-        success: false,
-        error: error.message,
-    })
+  return res.status(500).send({
+    message: 'something went wrong',
+    status: 500,
+    success: false,
+    error: error.message,
+  })
 }
