d4e34228a8a11bd72d78ad0047d114b62be1db73
Fhave <omosiyobo@gmail.com>
2026-07-14 16:40:23 +0100

    refactor: mask sensitive information

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,13 +1,32 @@
 const CustomError = require('./../helpers/error')
-const now = new Date();
-const watTime = new Intl.DateTimeFormat('en-GB', {
-  dateStyle: 'short',
-  timeStyle: 'medium',
-  timeZone: 'Africa/Lagos'
-}).format(now);
+
+function maskSensitiveData(obj) {
+  if (!obj || typeof obj !== 'object') return obj;
+
+  const cloned = Array.isArray(obj) ? [...obj] : { ...obj };
+  const sensitiveKeys = ['password', 'confirmpassword', 'token', 'cvv', 'pin', 'oldpassword', 'newpassword'];
+
+  for (const key in cloned) {
+    if (Object.prototype.hasOwnProperty.call(cloned, key)) {
+      if (sensitiveKeys.includes(key.toLowerCase())) {
+        cloned[key] = '[CONCEALED]';
+      } else if (typeof cloned[key] === 'object') {
+        cloned[key] = maskSensitiveData(cloned[key]);
+      }
+    }
+  }
+  return cloned;
+}
 
 module.exports = function (error, req, res, _next) {
   error = error.instance ? error.instance : error
+
+  const now = new Date();
+  const watTime = new Intl.DateTimeFormat('en-GB', {
+    dateStyle: 'short',
+    timeStyle: 'medium',
+    timeZone: 'Africa/Lagos'
+  }).format(now);
 
   console.error('----------------------------------------------------------------')
   console.error(
@@ -17,7 +36,11 @@ module.exports = function (error, req, res, _next) {
     req?.user?.id,
     '\n\n'
   )
-  console.error(req.body)
+
+  if (req.body) {
+    console.error('Request Body:', maskSensitiveData(req.body));
+  }
+
   console.error(error)
   console.error('----------------------------------------------------------------')
 
